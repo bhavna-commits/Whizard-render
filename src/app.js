ed9321097fb60479c0f6eb353b22ace0d8990072
Dharmesh Shimpi <dharmesh@viralpitch.co>
2025-03-26 01:09:51 +0530

    static

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 import express from "express";
 import sessionMiddleware from "./middleWares/sessionHandler.js";
 import corsMiddleware from "./middleWares/cors.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -13,8 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(corsMiddleware);
 
-app.set("views", path.join(__dirname, "..", "views"));
-app.set("view engine", "ejs");
 // app.set("trust proxy", 1);
 
 app.locals.makeRange = (start, end) => {
