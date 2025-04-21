63001ff4eb93d2cd38a04337d0a45f94129f1bfe
Dharmesh Shimpi <dharmesh@viralpitch.co>
2025-04-21 17:49:17 +0530

    d

--- src/controllers/Templates/template.controller.js
+++ src/controllers/Templates/template.controller.js
@@ -664,6 +664,8 @@ export const getFaceBookTemplates = async (req, res) => {
 			return map;
 		}, {});
 
+		console.log(fbTemplateMap);
+
 		// Prepare bulk operations to minimize database round-trips
 		const bulkOps = mongoTemplates
 			.map((mongoTpl) => {
