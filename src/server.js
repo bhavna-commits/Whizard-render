1335cdb19908b334271134235be47dae8494b62c
Dharmesh Shimpi <dharmesh@viralpitch.co>
2025-05-13 16:25:03 +0530

    vc

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 import app from "./routes.app.js";
 import { connectDB } from "./config/db.js";
 import { Server } from "socket.io";
-import https from "https";
+import https from "http";
 import {
 	getUsers,
 	sendMessages,
@@ -16,17 +16,17 @@ import { agenda } from "./config/db.js";
 
 connectDB();
 
-const __dirname = path.resolve();
-// Path to your certificate and private key
-const privateKey = fs.readFileSync(
-	path.join(__dirname, "certs", "private-key-no-passphrase.pem"),
-	"utf8",
-);
-const certificate = fs.readFileSync(
-	path.join(__dirname, "certs", "certificate.pem"),
-	"utf8",
-);
-const credentials = { key: privateKey, cert: certificate };
+// const __dirname = path.resolve();
+// // Path to your certificate and private key
+// const privateKey = fs.readFileSync(
+// 	path.join(__dirname, "certs", "private-key-no-passphrase.pem"),
+// 	"utf8",
+// );
+// const certificate = fs.readFileSync(
+// 	path.join(__dirname, "certs", "certificate.pem"),
+// 	"utf8",
+// );
+// const credentials = { key: privateKey, cert: certificate };
 
 agenda.on("ready", async () => {
 	console.log("Agenda started");
@@ -39,7 +39,7 @@ process.on("SIGTERM", async () => {
 	process.exit(0);
 });
 
-const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(app);
 
 const io = new Server(httpsServer, {
 	cors: { origin: "*" },
@@ -90,10 +90,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 5001;
-
-// https.createServer(credentials, app).listen(PORT, "0.0.0.0", () => {
-// 	console.log(`Server running securely on https://localhost:${PORT}`);
-// });
 
 httpsServer.listen(PORT, "0.0.0.0", () => {
 	console.log(`Server running securely on http://localhost:${PORT}`);
