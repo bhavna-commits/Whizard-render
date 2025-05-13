c8b88a15c6a7165ca714bf35d30c6bfdad5ca3f8
Dharmesh Shimpi <dharmesh@viralpitch.co>
2025-05-13 16:06:51 +0530

    gfd

--- src/server.js
+++ src/server.js
@@ -3,24 +3,29 @@ dotenv.config();
 import app from "./routes.app.js";
 import { connectDB } from "./config/db.js";
 import { Server } from "socket.io";
-import http from "http";
-import { getUsers, sendMessages, searchUsers, getSingleChat } from "./controllers/Chats/chats.controller.js";
+import https from "https";
+import {
+	getUsers,
+	sendMessages,
+	searchUsers,
+	getSingleChat,
+} from "./controllers/Chats/chats.controller.js";
 import path from "path";
 import { agenda } from "./config/db.js";
 
 connectDB();
 
-// const __dirname = path.resolve();
-// // Path to your certificate and private key
-// const privateKey = fs.readFileSync(
-// 	path.join(__dirname, "certs", "private-key-no-passphrase.pem"),
-// 	"utf8",
-// );
-// const certificate = fs.readFileSync(
-// 	path.join(__dirname, "certs", "certificate.pem"),
-// 	"utf8",
-// );
-// const credentials = { key: privateKey, cert: certificate };
+const __dirname = path.resolve();
+// Path to your certificate and private key
+const privateKey = fs.readFileSync(
+	path.join(__dirname, "certs", "private-key-no-passphrase.pem"),
+	"utf8",
+);
+const certificate = fs.readFileSync(
+	path.join(__dirname, "certs", "certificate.pem"),
+	"utf8",
+);
+const credentials = { key: privateKey, cert: certificate };
 
 agenda.on("ready", async () => {
 	console.log("Agenda started");
@@ -33,40 +38,39 @@ process.on("SIGTERM", async () => {
 	process.exit(0);
 });
 
-const httpsServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
 const io = new Server(httpsServer, {
 	cors: { origin: "*" },
 });
 
 io.on("connection", (socket) => {
-
 	console.log("User connected:", socket.id);
 
 	socket.on("initial-users_idal_com", async (data) => {
 		console.log(data);
 		let res = {};
 		try {
-			if (data["type"] == "send-message") {
+			if (data["type"] === "send-message") {
 				console.log(data);
 				try {
 					res = await sendMessages(data);
 				} catch (e) {
 					console.log(e);
 				}
-			} else if (data["type"] == "getUsers") {
+			} else if (data["type"] === "getUsers") {
 				try {
 					res = await getUsers(data);
 				} catch (e) {
 					console.log(e);
 				}
-			} else if (data["type"] == "getSingleChat") {
+			} else if (data["type"] === "getSingleChat") {
 				try {
 					res = await getSingleChat(data);
 				} catch (e) {
 					console.log(e);
 				}
-			} else if (data["type"] == "searchUsers") {
+			} else if (data["type"] === "searchUsers") {
 				try {
 					res = await searchUsers(data);
 				} catch (e) {
@@ -89,7 +93,6 @@ const PORT = process.env.PORT || 5001;
 // https.createServer(credentials, app).listen(PORT, "0.0.0.0", () => {
 // 	console.log(`Server running securely on https://localhost:${PORT}`);
 // });
-
 
 httpsServer.listen(PORT, "0.0.0.0", () => {
 	console.log(`Server running securely on http://localhost:${PORT}`);
