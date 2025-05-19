2fa738b2a5031b3d3bb81f4c4bb63d781a0a0bcd
Dharmesh Shimpi <dharmesh@viralpitch.co>
2025-05-19 14:19:00 +0530

    fd

--- src/backEnd-Routes/facebook.backEnd.routes.js
+++ src/backEnd-Routes/facebook.backEnd.routes.js
@@ -96,6 +96,7 @@ router.post("/auth_code", async (req, res) => {
 				$push: {
 					FB_PHONE_NUMBERS: {
 						phone_number_id,
+						selected: true,
 					},
 				},
 			},
@@ -130,198 +131,6 @@ router.post("/auth_code", async (req, res) => {
 		});
 	}
 });
-
-// router.post("/webhook", async (req, res) => {
-// 	try {
-// 		const { entry } = req.body;
-
-// 		for (const entryItem of entry) {
-// 			const wabaId = entryItem.id;
-
-// 			if (entryItem.changes) {
-// 				const change = entryItem.changes[0];
-// 				const messagingEvent = change.value;
-// 				console.log(JSON.stringify(entryItem));
-// 				// Handle status events
-// 				if (messagingEvent.statuses) {
-// 					const fbPhoneId =
-// 						messagingEvent.metadata?.phone_number_id || "";
-// 					const statusEvent = messagingEvent.statuses[0];
-// 					console.log(statusEvent);
-// 					const {
-// 						id: messageId,
-// 						status,
-// 						timestamp,
-// 						recipient_id: recipientPhone,
-// 						errors,
-// 					} = statusEvent;
-
-// 					const tempStatus = new TempStatus({
-// 						fbPhoneId,
-// 						wabaId,
-// 						messageId,
-// 						status: status.toUpperCase(),
-// 						timestamp: timestamp * 1000,
-// 						recipientPhone,
-// 						error: errors || [],
-// 						// rawData: statusEvent,
-// 					});
-// 					await tempStatus.save();
-// 				}
-
-// 				// Handle template rejections
-// 				if (
-// 					messagingEvent.event === "REJECTED" ||
-// 					messagingEvent.status === "REJECTED"
-// 				) {
-// 					console.log(messagingEvent);
-// 					let templateId,
-// 						templateName,
-// 						templateLanguage,
-// 						rejectedReason;
-
-// 					if (messagingEvent.event === "REJECTED") {
-// 						templateId = messagingEvent.message_template_id;
-// 						templateName = messagingEvent.message_template_name;
-// 						templateLanguage =
-// 							messagingEvent.message_template_language;
-// 						rejectedReason = messagingEvent.reason;
-// 					} else if (messagingEvent.status === "REJECTED") {
-// 						templateId = entryItem.id;
-// 						rejectedReason = "Unknown reason";
-// 					}
-
-// 					const tempRejection = new TempTemplateRejection({
-// 						wabaId,
-// 						templateId: String(templateId),
-// 						templateName,
-// 						templateLanguage,
-// 						rejectedReason,
-// 						// rawData: messagingEvent,
-// 					});
-// 					await tempRejection.save();
-// 				}
-
-// 				if (messagingEvent?.messages) {
-// 					const fbPhoneId =
-// 						messagingEvent.metadata?.phone_number_id || "";
-// 					const name = messagingEvent.contacts?.[0]?.profile?.name;
-// 					const messageEvent = messagingEvent.messages[0];
-// 					const {
-// 						id: messageId,
-// 						from: senderPhone,
-// 						timestamp,
-// 						type,
-// 						text,
-// 						image,
-// 						video,
-// 						document,
-// 						audio,
-// 					} = messageEvent;
-
-// 					// Determine mediaUrl if the message contains a file
-// 					let mediaId = "";
-// 					if (type === "image" && image?.id) {
-// 						mediaId = image.id;
-// 					} else if (type === "video" && video?.id) {
-// 						mediaId = video.id;
-// 					} else if (type === "document" && document?.id) {
-// 						mediaId = document.id;
-// 					} else if (type === "audio" && audio?.id) {
-// 						mediaId = audio.id;
-// 					}
-
-// 					// Build and save the TempMessage
-// 					const tempMessage = new TempMessage({
-// 						name,
-// 						wabaId,
-// 						messageId,
-// 						from: senderPhone,
-// 						timestamp: timestamp * 1000,
-// 						type,
-// 						text:
-// 							text ||
-// 							image?.caption ||
-// 							video?.caption ||
-// 							document?.caption ||
-// 							audio?.caption,
-// 						mediaId,
-// 						fbPhoneId,
-// 						// rawData: messageEvent,
-// 					});
-// 					await tempMessage.save();
-// 				}
-// 			}
-// 		}
-
-// 		res.status(200).send("EVENT_RECEIVED");
-// 	} catch (err) {
-// 		console.error("Error processing webhook:", err);
-// 		res.status(200).send("EVENT_RECEIVED");
-// 	}
-// });
-
-function sendsocket(
-	agentslist,
-	userno,
-	text,
-	timestamp,
-	name,
-	mediaId,
-	type,
-	fbPhoneId,
-) {
-	console.log(agentslist);
-	if (agentslist.length > 0) {
-		for (var i = 0; i < agentslist.length; i++) {
-			let bptId = "new-message_idal_com_" + userno + agentslist[i];
-			console.log(bptId);
-
-			let lastmessage = text?.body || text;
-			if (type === "text") {
-				type = "";
-			}
-			if (type) {
-				lastmessage = type;
-			}
-
-			let data1 = {
-				filter_data: {},
-				msg: text?.body || text,
-				timestamp: timestamp,
-				status: "replied",
-				username: name,
-				wa_id: userno,
-				media_message: mediaId,
-				media_type: type,
-			};
-
-			let data2 = {
-				filter_data: {},
-				sent: 1,
-				lastmessage: lastmessage,
-				wa_id: userno,
-				status: 0,
-				username: name,
-				name: name,
-				keyId: fbPhoneId,
-				usertimestmp: timestamp,
-				is_read: false,
-				media_message: mediaId,
-				media_type: type,
-			};
-
-			let bptId2 = "MainRefresh_initial-users_" + agentslist[i];
-			//console.log(bptId)
-			console.log(data1);
-
-			//console.log(bptId2)
-			console.log(data2);
-			io.emit(bptId, data1);
-			io.emit(bptId2, data2);
-		}
-	}
-}
 
 router.post("/webhook", async (req, res) => {
 	try {
@@ -527,6 +336,68 @@ router.post("/webhook", async (req, res) => {
 		res.status(200).send("EVENT_RECEIVED");
 	}
 });
+
+function sendsocket(
+	agentslist,
+	userno,
+	text,
+	timestamp,
+	name,
+	mediaId,
+	type,
+	fbPhoneId,
+) {
+	console.log(agentslist);
+	if (agentslist.length > 0) {
+		for (var i = 0; i < agentslist.length; i++) {
+			let bptId = "new-message_idal_com_" + userno + agentslist[i];
+			console.log(bptId);
+
+			let lastmessage = text?.body || text;
+			if (type === "text") {
+				type = "";
+			}
+			if (type) {
+				lastmessage = type;
+			}
+
+			let data1 = {
+				filter_data: {},
+				msg: text?.body || text,
+				timestamp: timestamp,
+				status: "replied",
+				username: name,
+				wa_id: userno,
+				media_message: mediaId,
+				media_type: type,
+			};
+
+			let data2 = {
+				filter_data: {},
+				sent: 1,
+				lastmessage: lastmessage,
+				wa_id: userno,
+				status: 0,
+				username: name,
+				name: name,
+				keyId: fbPhoneId,
+				usertimestmp: timestamp,
+				is_read: false,
+				media_message: mediaId,
+				media_type: type,
+			};
+
+			let bptId2 = "MainRefresh_initial-users_" + agentslist[i];
+			// //console.log(bptId)
+			// console.log(data1);
+
+			// //console.log(bptId2)
+			// console.log(data2);
+			// io.emit(bptId, data1);
+			// io.emit(bptId2, data2);
+		}
+	}
+}
 
 async function scheduleAllRefreshJobs() {
 	const now = new Date();
